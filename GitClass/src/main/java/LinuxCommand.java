3e08fd633bacf466a00d012fef3a4c72d07fa348
my name <you@examplejtge.com>
2019-04-17 18:43:11 -0400

    added project

---
+++ GitClass/src/main/java/LinuxCommand.java
@@ -0,0 +1,14 @@
+
+public class LinuxCommand 
+{
+	/*cd-----change directory
+	pwd----present work directory
+	ls---to see what folder contains
+	ls -l----to see what folder contains with details
+	mkdir---to create folder
+	touch---to create a file
+	nano/vi--is a editor like notepad
+	cat---to see file
+	grep---to get string value from file*/
+
+}
